c16d0b8c74518658001d9b736947aaf70545062e
marta-canizares <marta.s.canizares@gmail.com>
2020-04-20 22:46:26 +0200

    first commit

---
+++ ejercicio 5/ejercicio5.js
@@ -0,0 +1,26 @@
+let alumno = prompt('Que número de alumno quiere saber?');
+
+let alumnos = ["Marta", "Mariandreai", "Dario", "Christian", "Ariel", "Eduardo", "Alejandro",
+    "Samuel", "Alex", "Antonio"];
+
+if (alumno == 0) {
+    console.log(alumnos[0]);
+} else if (alumno == 1) {
+    console.log(alumnos[1]);
+} else if (alumno == 2) {
+    console.log(alumnos[2]);
+} else if (alumno == 3) {
+    console.log(alumnos[3]);
+} else if (alumno == 4) {
+    console.log(alumnos[4]);
+} else if (alumno == 5) {
+    console.log(alumnos[5]);
+} else if (alumno == 6) {
+    console.log(alumnos[6]);
+} else if (alumno == 7) {
+    console.log(alumnos[7]);
+} else if (alumno == 8) {
+    console.log(alumnos[8]);
+} else if (alumno == 9) {
+    console.log(alumnos[9]);
+}
---
+++ ejercicio 8/ejercicio8.js
@@ -0,0 +1,9 @@
+const tiempo = prompt('Que día hace hoy?')
+
+if (tiempo == 1) {
+    alert('Baja probabilidad de lluvia')
+} else if (tiempo == 2) {
+    alert('Probabilidad de lluvia del 60%')
+} else if (tiempo == 3) {
+    alert('Probabilidad de lluvia del 90%')
+}
